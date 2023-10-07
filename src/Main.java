e94c1c2ae6361c481e109ab2bb9c8c83bc22522f
1123Alessandro <alessandroandyrei@gmail.com>
2023-10-07 23:18:29 +0800

    initial regex

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
-import mod.*;
+import toolset.*;
+import java.util.regex.*;
+import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("test");
-        Andy obj = new Andy();
-        System.out.println(obj.x);
+    public static void main(String[] args) throws IOException {
+        CodeReader obj = new CodeReader("test.txt");
+        String text = obj.output();
+        System.out.println("--------------------------------------------------");
+        System.out.println(text);
+        System.out.println("--------------------------------------------------");
+
+        Pattern p = Pattern.compile("true");
+        Matcher m = p.matcher(text);
+        m.find();
+        // System.out.println(m.group());
+        System.out.println();
     }
 }
