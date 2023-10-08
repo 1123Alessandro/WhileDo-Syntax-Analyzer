bcead531a0648ad059886be74ce606ce6cce4def
1123Alessandro <alessandroandyrei@gmail.com>
2023-10-08 17:44:26 +0800

    Main java, messy status

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,26 @@ public class Main {
     public static void main(String[] args) throws IOException {
         CodeReader obj = new CodeReader("test.txt");
         String text = obj.output();
-        System.out.println("--------------------------------------------------");
+        System.out.println("Original Text:------------------------------------");
         System.out.println(text);
         System.out.println("--------------------------------------------------");
 
-        Pattern p = Pattern.compile("true");
-        Matcher m = p.matcher(text);
-        m.find();
+        // Pattern p = Pattern.compile("\n");
+        // Matcher m = p.matcher(text);
+        // m.find();
         // System.out.println(m.group());
-        System.out.println();
+        // System.out.println(m.replaceAll(""));
+
+
+        RegExer re = new RegExer();
+        // System.out.println(rr.findPattern("t.+e", text).group());
+        String[] matches = re.findPattern("(t.+e)|(h.*d)", text);
+        for (String s : matches) {
+            System.out.println(s);
+        }
+
+        String hhh = re.replace("(t.+e)|(h.*d)", "000", text);
+        System.out.println(hhh);
+        // rr.findPattern("true", text);
     }
 }
