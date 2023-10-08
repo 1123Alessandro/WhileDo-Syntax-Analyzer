04a8e05419f85fcda6d4b66fac06f4ebba71516e
1123Alessandro <alessandroandyrei@gmail.com>
2023-10-08 21:26:32 +0800

    cleaned up mess and added TODO tasks

--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,36 @@ import java.io.*;
 
 public class Main {
 
+    // DONE: parse the main structure of the while loop
+    public static String[] findWhile(RegExer re, String text) {
+        String pattern = "while [(].+[)] [{].*[}]";
+        String[] matches = re.findPattern(pattern, text);
+        return matches;
+    }
+
+    // TODO: parse the logic area of the while loop
+    public static String[] findLogic(RegExer re, String text) {
+        return null;
+    }
+
+    // TODO: parse the content area of the while loop to include nested whiles
+    public static String[] findContent(RegExer re, String text) {
+        return null;
+    }
+
     public static void main(String[] args) throws IOException {
         CodeReader obj = new CodeReader("test.txt");
         String text = obj.output();
-        System.out.println("Original Text:------------------------------------");
+        System.out.println("Original code:------------------------------------");
         System.out.println(text);
         System.out.println("--------------------------------------------------");
 
-        // Pattern p = Pattern.compile("\n");
-        // Matcher m = p.matcher(text);
-        // m.find();
-        // System.out.println(m.group());
-        // System.out.println(m.replaceAll(""));
-
-
+        // TODO: pattern of the whole program
         RegExer re = new RegExer();
-        // System.out.println(rr.findPattern("t.+e", text).group());
-        String[] matches = re.findPattern("(t.+e)|(h.*d)", text);
+        String[] matches = findWhile(re, text);
         for (String s : matches) {
             System.out.println(s);
         }
 
-        String hhh = re.replace("(t.+e)|(h.*d)", "000", text);
-        System.out.println(hhh);
-        // rr.findPattern("true", text);
     }
 }
