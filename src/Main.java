03c1e46fe3f1e29ee8b2012d21cfea8e7efb5d26
edsnjavier <reivajnosidexyz@gmail.com>
2023-10-09 22:24:50 +0800

    updated main and regexer

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,42 @@ public class Main {
     }
 
     // TODO: parse the logic area of the while loop
-    public static String[] findLogic(RegExer re, String text) {
-        return null;
+    public static boolean findLogic(RegExer re, String text) {
+        String pattern = "while\\s*(\\([^)]*\\))\\s*\\{";
+        String m = re.extractWhileCondition(pattern, text);
+        boolean flag = false;
+
+        m = m.replaceAll("\\s+", "");
+        System.out.println(m);
+
+        String firstP = m.substring(0, 1);
+        String secondP = m.substring(m.length() - 1);
+        //System.out.println(firstP);
+        //System.out.println(secondP);
+        if(firstP.equals("(") && secondP.equals(")")) {
+            flag = true;
+        }
+
+        // Check if each part contains a variable name followed by a comparison operator
+        if(m.matches("[a-zA-Z_][a-zA-Z0-9_]*[<>!=]=?[<>]?[0-9]+")) {
+            flag = true;
+        }
+        else if(m.matches("[a-zA-Z_][a-zA-Z0-9_]*[<>!=]=?[<>]?[a-zA-Z_][a-zA-Z0-9_]*")) {
+            flag = true;
+        }
+
+        return flag;
+
+        //return matches;
+    }
+
+    public static String validityChecker(boolean flag) {
+        if(flag == true) {
+            return "Valid Syntax";
+        }
+        else {
+            return "Invalid Syntax";
+        }
     }
 
     // TODO: parse the content area of the while loop to include nested whiles
@@ -30,10 +64,17 @@ public class Main {
 
         // TODO: pattern of the whole program
         RegExer re = new RegExer();
-        String[] matches = findWhile(re, text);
+        /*String[] matches = findWhile(re, text);
         for (String s : matches) {
             System.out.println(s);
-        }
+        }*/
+        /*String[] matches = findLogic(re, text);
+        for (String s : matches) {
+            System.out.println(s);
+        }*/
+        
+        String conclu = validityChecker(findLogic(re, text));
+        System.out.println("Valid or not: " + conclu);
 
     }
 }
